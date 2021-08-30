4821b9361c8f330b342a0ace29cb728fa1d091ba
DanishPandita <danishpandita1999@gmail.com>
2021-08-30 19:42:55 +0530

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import 'tailwindcss/tailwind.css'
+/** @format */
 
+import Head from "next/head";
+import "tailwindcss/tailwind.css";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
